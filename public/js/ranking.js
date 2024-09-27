ae54afe02fd49204e2520105115a253c85a133c4
José Hernanes <68444005+devjoseh@users.noreply.github.com>
2024-09-27 19:05:32 -0300

    d

--- public/js/ranking.js
+++ public/js/ranking.js
@@ -3,10 +3,24 @@ document.getElementById('refreshBtn').addEventListener('click', async function()
 
     try {
         // const response = await fetch(`/api/getValue/App Fecarte/`);
-        const response = await fetch(`/api/getValue/?path=App Fecarte`);
+        const response = await fetch(`/api/getValue/?path=App Fecarte/`);
         console.log(response)
     } catch (error) {
         showNotification("Erro: " + error.message, "error");
         console.error(error)
     }
 });
+
+function showNotification(message, type) {
+    const notification = document.getElementById('notification');
+    notification.textContent = message;
+    notification.className = `notification show ${type}`;
+    notification.classList.remove('hidden');
+
+    setTimeout(() => {
+        notification.classList.remove('show');
+        setTimeout(() => {
+            notification.classList.add('hidden');
+        }, 500);
+    }, 3000);
+}
