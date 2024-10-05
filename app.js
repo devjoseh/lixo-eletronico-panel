a9ce3843aa6eaaac1b1317e7026b59d22a5e5431
José Hernanes <68444005+devjoseh@users.noreply.github.com>
2024-10-05 20:59:03 -0300

    fix: Fixed the bug of the api not loading

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 import express from "express";
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+
+const apiPath = path.join(__dirname, 'api');
+fs.readdirSync(apiPath).forEach((file) => {
+    if (file.endsWith('.js')) {
+        const route = `/api/${file.replace('.js', '')}`;
+        const modulePath = `./api/${file}`;
+        
+        import(modulePath).then((module) => {
+            app.use(route, module.default);
+        }).catch((err) => {
+            console.error(`Erro ao carregar o módulo ${file}:`, err);
+        });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
