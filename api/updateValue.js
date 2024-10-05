b77e98da43a2fb3f7d1acd3115b9cd86412e3fc8
José Hernanes <68444005+devjoseh@users.noreply.github.com>
2024-10-05 20:53:01 -0300

    feat: Performance upgrade and adaptability

--- api/updateValue.js
+++ api/updateValue.js
@@ -1,15 +1,15 @@
-import { getValue, updateValue } from '../lib/functions.js';
+import { updateValue } from '../lib/functions.js';
 
 export default async function handler(req, res) {
     const { id, data } = req.body;
 
     try {
-        const existingData = await getValue(`App Fecarte/${id}`, null);
-        const updatedItems = existingData?.itens ? [...existingData.itens, ...data] : data;
-
-        await updateValue(`App Fecarte/${id}`, { itens: updatedItems });
-        res.json({ message: 'Itens adicionados com sucesso!' });
+        const path = `App Fecarte/${id}`
+        
+        await updateValue(path, data);
+        console.info(`Atualização realizada com sucesso em ${path}, dados:`, data)
+        res.json({ message: 'Atualização realizada com sucesso.' });
     } catch (error) {
-        res.status(500).json({ message: 'Erro ao adicionar os itens', error });
+        res.status(500).json({ message: 'Erro ao adicionar atualizar uma informação', error });
     }
 }
